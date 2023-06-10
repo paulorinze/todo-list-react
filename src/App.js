d7e39b28984ea9356a774b93afa53ab0e10d2430
paulorinze <paulo.rinze@uzambeze.onmicrosoft.com>
2023-06-10 02:50:19 +0100

    Code improvements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  //creating blank state 
+  const [todoList,setTodoList] = useState([]);
+  //Tracking the value inside the input
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    //creating a new TodoList array
+    setTodoList([...todoList, newTask]);
+  };
+
   return (
     <div className="App">
-        Hello Motherfucker
+        <div className="addTasks">
+          <input onChange={handleChange}/>
+          <button onClick={addTask}> Add Task </button>
+        </div>
+        <div className="list"></div>
+        {todoList.map((task) => {
+          return <h1>{task}</h1>;
+        })}
     </div>
   );
 }
